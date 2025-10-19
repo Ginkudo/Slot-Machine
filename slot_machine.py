cd73b8d7811cb0f7de299133974f72bdf40a1ca0
Jimmy Phan <phanjimmy02@gmail.com>
2025-10-19 17:28:43 -0400

    deposit function initial commit

---
+++ slot_machine.py
@@ -0,0 +1,14 @@
+def deposit(): #user input
+    while True:
+        amount = input('What is your deposit? $')
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print('Amount must be greater than 0.')
+        else:
+            print('Please enter a number.')
+    return amount
+
+deposit()
