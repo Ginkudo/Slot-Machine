9cd0be9a8f57618ff08f29d9561d3a5208abc9d8
Jimmy Phan <phanjimmy02@gmail.com>
2026-01-23 15:50:16 -0500

    new line

--- slot_machine.py
+++ slot_machine.py
@@ -32,9 +32,11 @@ def print_slot_machine(columns): #prints columns and rows to spin
     for row in range(len(columns[0])):
         for i, column in enumerate(columns):
             if i != len(columns) - 1:
-                print(column[row], '|')
+                print(column[row], '|', end=' | ')
             else:
-                print(column[row])
+                print(column[row], end='')
+
+        print()
 
 def deposit(): #user input
     while True:
